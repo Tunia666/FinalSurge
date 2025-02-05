c1cd11d25b064ab2a66565cd05c731fe6b5504fe
truda <truda1999@gmail.com>
2025-02-05 11:41:13 +0300

    Reworked homework from 05/02

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,24 +2,30 @@ package pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class LoginPage {
-    public LoginPage openAuthorization(){
+    private final SelenideElement loginButton = $x("//span[contains(text(), 'Log In')]");
+    private final SelenideElement emailInput = $("input[name='email']");
+    private final SelenideElement passwordInput = $("input[name='password']");
+    private final SelenideElement checkBox = $("div.check-option__box");
+    private final SelenideElement submitButton = $("button[type='submit'][class*='action-button--primary']");
+
+    public LoginPage openAuthorization() {
         open("https://www.finalsurge.com/");
-        $x("//span[contains(text(), 'Log In')]").click();
+        loginButton.click();
         Selenide.sleep(2000);
         return this;
     }
-    public LoginPage loginUser(String login, String password){
-        $("input[name='email']").shouldBe(Condition.visible).setValue(login);
-        $("input[name='password']").shouldBe(Condition.visible).setValue(password);
-        $("div.check-option__box").shouldBe(Condition.visible).click();
-        $("button[type='submit'][class*='action-button--primary']")
-                .shouldBe(Condition.visible)
-                .click();
+
+    public LoginPage loginUser(String login, String password) {
+        emailInput.shouldBe(Condition.visible).setValue(login);
+        passwordInput.shouldBe(Condition.visible).setValue(password);
+        checkBox.shouldBe(Condition.visible).click();
+        submitButton.shouldBe(Condition.visible).click();
         Selenide.sleep(2000);
         return this;
     }
--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,35 +1,46 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationPage {
+    SelenideElement registrationButton = $("a[href='/signup'].action-button--primary");
+    SelenideElement beginAccountHeader = $("h1.h4");
+    SelenideElement checkBox = $("div.check-option__box[data-v-21593069]");
+    SelenideElement submitButton = $("button.action-button--primary[type='submit']");
+    SelenideElement createAthleteAccountHeader = $("h1.h4.sign-up-form-inputs__label");
+    SelenideElement firstNameInput = $("input[name='firstName'].text-field--required");
+    SelenideElement lastNameInput = $("input[name='lastName'].text-field--required");
+    SelenideElement emailInput = $("input[name='_none'].text-field--required");
+    SelenideElement passwordInput = $("input[name='new-password'].text-field--required");
+    SelenideElement repeatPasswordInput = $("input[name='repeatPassword'].text-field--required");
+    SelenideElement errorMassage = $("div.field-footer__error");
     public RegistrationPage openRegistration() {
         open("https://www.finalsurge.com/");
-        $("a[href='/signup'].action-button--primary").shouldBe(visible).click();
-        $("h1.h4").shouldHave(Condition.text("Begin your account as..."));
-        $("div.check-option__box[data-v-21593069]").shouldBe(Condition.visible)
-                .click();
-        $("button.action-button--primary[type='submit']").shouldBe(Condition.visible).click();
-        $("h1.h4.sign-up-form-inputs__label").shouldHave(Condition.text("Create Athlete Account"));
+        registrationButton.shouldBe(visible).click();
+        beginAccountHeader.shouldHave(Condition.text("Begin your account as..."));
+        checkBox.shouldBe(Condition.visible).click();
+        submitButton.shouldBe(Condition.visible).click();
+        createAthleteAccountHeader.shouldHave(Condition.text("Create Athlete Account"));
         return this;
     }
 
     public RegistrationPage registrationUser(String firstName, String lastName, String email, String password, String repeatPassword) {
-        $("input[name='firstName'].text-field--required").shouldBe(visible).setValue(firstName);
-        $("input[name='lastName'].text-field--required").shouldBe(visible).setValue(lastName);
-        $("input[name='_none'].text-field--required").shouldBe(visible).setValue(email);
-        $("input[name='new-password'].text-field--required").shouldBe(visible).setValue(password);
-        $("input[name='repeatPassword'].text-field--required").shouldBe(visible).setValue(repeatPassword);
-        $("button.action-button--primary[type='submit']").shouldBe(visible).click();
+        firstNameInput.shouldBe(visible).setValue(firstName);
+        lastNameInput.shouldBe(visible).setValue(lastName);
+        emailInput.shouldBe(visible).setValue(email);
+        passwordInput.shouldBe(visible).setValue(password);
+        repeatPasswordInput.shouldBe(visible).setValue(repeatPassword);
+        submitButton.shouldBe(visible).click();
         return this;
     }
 
     public RegistrationPage checkErrorMessage(String expectedError) {
-        $("div.field-footer__error").shouldBe(visible).shouldHave(text(expectedError));
+        errorMassage.shouldBe(visible).shouldHave(text(expectedError));
         return this;
     }
 }
