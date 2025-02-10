b82c07c207df2b43e491ec9b57848a132fd1c334
truda <truda1999@gmail.com>
2025-02-10 16:47:55 +0300

    add headless in LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,13 +1,15 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import test.BaseTest;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
 
-public class LoginPage {
+public class LoginPage extends BaseTest {
     private final SelenideElement loginButton = $x("//span[contains(text(), 'Log In')]");
     private final SelenideElement emailInput = $("input[name='email']");
     private final SelenideElement passwordInput = $("input[name='password']");
@@ -15,6 +17,7 @@ public class LoginPage {
     private final SelenideElement submitButton = $("button[type='submit'][class*='action-button--primary']");
 
     public LoginPage openAuthorization() {
+        Configuration.headless = true;
         open("https://www.finalsurge.com/");
         loginButton.click();
         Selenide.sleep(2000);
@@ -24,7 +27,7 @@ public class LoginPage {
     public LoginPage loginUser(String login, String password) {
         emailInput.shouldBe(Condition.visible).setValue(login);
         passwordInput.shouldBe(Condition.visible).setValue(password);
-        checkBox.shouldBe(Condition.visible).click();
+        //checkBox.shouldBe(Condition.visible).click();
         submitButton.shouldBe(Condition.visible).click();
         Selenide.sleep(2000);
         return this;
