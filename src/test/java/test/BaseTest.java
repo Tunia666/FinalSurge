1e06071348834b6b1d5d1c116b37ab3af8f6858b
truda <truda1999@gmail.com>
2025-02-10 13:53:14 +0300

    add headless

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -6,8 +6,9 @@ import org.testng.annotations.BeforeMethod;
 public class BaseTest {
     @BeforeMethod
     public void setUp(){
+        Configuration.headless = true;
         Configuration.baseUrl = "https://www.finalsurge.com/";
-        Configuration.browser = "safari";
+        Configuration.browser = "chrome";
         Configuration.timeout = 20000;
         Configuration.clickViaJs = true; //прожимает кнопки, которые скрываются за элементам
     }
